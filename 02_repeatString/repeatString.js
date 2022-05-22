0ebdc014cb8cd0c2458da00a4fdb5216855bc3d1
Jaryd Yia <jjaryd@Gmail.com>
2022-05-22 17:23:01 -0400

    Finished exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,22 @@
-const repeatString = function() {
+let phrase = "bleep"
+let repeater = 10
+
+const repeatString = function(phrase, repeater) {
+    // if(phrase === ""){
+    //     return ""
+    // }
+    if (repeater <0 ){
+        return "ERROR";
+    }
+
+    let phraseCompletion = "";
+
+    for(i = 0; i < repeater ; i++){
+        phraseCompletion = phraseCompletion + phrase
+    }
+
+    return phraseCompletion
+
 
 };
 
