f49724cddd3d6e9c72974cfe6ccc2129f1d10357
Jaryd Yia <jjaryd@Gmail.com>
2022-05-22 21:12:40 -0400

    Completed exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,23 @@
-const reverseString = function() {
+let string = "hello"
 
+const reverseString = function(string) {
+    const reverseArr = [];
+    let stringReverse = "";
+    
+
+    console.log(typeof(reverseArr))
+
+    for ( i = string.length-1; i >= 0; i--){
+        reverseArr.push(string[i])
+    } 
+    console.log(reverseArr)
+    console.log(typeof(reverseArr[0]))
+    console.log(reverseArr.join(""))// THIS MAKES IT SO SEPERATING EACH SECTION is not using a , 
+    return reverseArr.join("");
+
+
+
+    
 };
 
 // Do not edit below this line
