0cead1744efb61246d11b107ae253e65026f9a12
Jaryd Yia <jjaryd@Gmail.com>
2022-05-24 17:42:49 -0400

    Finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,26 @@
-const sumAll = function() {
+let num1 = 1;
+let num2 = 4
+const sumAll = function(num1, num2) {
+    let sum = 0 ;
+    
+    if( typeof(num1) != 'number' || typeof(num2) != 'number'){
+        return 'ERROR'
+    }if (typeof(num1)== 'string'   || typeof(num2) == 'string'){
+        return 'ERROR'
+    }else if (num1 < 0){
+        return 'ERROR'
+    }else if(num1> num2){
+        for(i = num2; i <=num1; i++){
+            sum+=i;
+        }
+    }else if(num1<num2){
+        for(i = num1; i <= num2; i++){
+            sum+=i;
+        }
+    }
+
+
+    return sum
 
 };
 
