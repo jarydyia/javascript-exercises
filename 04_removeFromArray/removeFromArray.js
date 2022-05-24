20513c7a99a3be743ad31a677f292aa26281befe
Jaryd Yia <jjaryd@Gmail.com>
2022-05-24 16:26:38 -0400

    done exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+
+const arrList = [1,2,3,4] 
+//ARGUMENT NOW IS #
+
+
+const removeFromArray = function(array,...args) {
+const newArray = [];
+
+array.forEach((item)=>{ //1,2,3,4
+    if(!args.includes(item)){ //IF FALSE IT WILL DO THIS ... make a new array with the numbers that arent in nte argument
+        newArray.push(item);
+    }
+    console.log(newArray)
+})
+
+return newArray
 
 };
 
