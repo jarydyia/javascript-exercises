25c67e8d75b01da299f9520122deb35ad77edf69
Jaryd Yia <jjaryd@Gmail.com>
2022-05-24 17:53:00 -0400

    Finish exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,15 @@
-const leapYears = function() {
+let year = 1980
+
+const leapYears = function(year) {
+if ((year % 400) == 0){
+    return true
+}else if((year % 100) == 0 && (year % 4)== 0){
+    return false
+}else if((year % 4)== 0 ){
+    return true
+}else{
+    return false
+}
 
 };
 
