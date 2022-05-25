f1253537fa5603d34fe63a2654cea578f905c2b0
Jaryd Yia <jjaryd@Gmail.com>
2022-05-24 20:03:54 -0400

    Finish exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const ftoc = function() {
-
+let tempF = 100;
+const ftoc = function(tempF) {
+let tempCel = (tempF -32) * (5/9);
+tempCel = Math.round(tempCel *10)/10
+return tempCel;
 };
 
-const ctof = function() {
+let tempC = 0
 
+const ctof = function(tempC) {
+let tempFar = tempC * (9/5) +32
+tempFar =   Math.round(tempFar * 10)/10
+return tempFar;
 };
 
 // Do not edit below this line
